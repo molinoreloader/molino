fe90ee7189f92f387dac0d596bff14e282346443
molinoreloader <iban.molino@reloadmedia.com.au>
2013-03-20 20:15:08 -0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,29 @@
+<html>
+<title>Personal Information</title>
+<body>
+<style>
+  .bg {
+	background-color:#A9F5E1;
+	}
+	.font {
+	color:white;
+	font-size:15px;
+	}
+</style>
+<h1>My Profile</h1>
+<table border="0" class="bg">
+<tr>
+<td class="font">Name:</td>
+<td>Iban M. Molino</td>
+<tr>
+<td class="font">Birthdate:</td>
+<td>July 15, 1991</td>
+<tr>
+<td class="font">Birth Place:</td>
+<td>Romblon, Romblon</td>
+<tr>
+<td class="font">Sex:</td>
+<td>Male</td>
+</table>
+</body>
+</html>
